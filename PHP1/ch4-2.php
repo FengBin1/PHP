61d7315e9168d469ad1d2026441f66e07a34e23e
huawei <mailhw@petalmail.com>
2024-03-27 08:40:02 +0800

    PHP

--- PHP1/ch4-2.php
+++ PHP1/ch4-2.php
@@ -11,37 +11,38 @@
 </head>
 <body>
 <?php
+// 定义一个函数用于生成年历
 function calendar($y)
 {
-    $w = date('w', strtotime("$y-1-1"));
+    $w = date('w', strtotime("$y-1-1")); // 获取指定年份的1月1日是星期几
     $html = '<div class="box">';
     // 输出12个月份的表格
     for ($m = 1; $m <= 12; ++$m) {
         $html .= '<table>';
-        $html .= '<tr><th colspan="7">' . $y . ' 年 ' . $m . ' 月</th></tr>';
+        $html .= '<tr><th colspan="7">' . $y . ' 年 ' . $m . ' 月</th></tr>'; // 表头显示年份和月份
         $html .= '<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td>
-                    <td>五</td><td>六</td></tr>';
+                    <td>五</td><td>六</td></tr>'; // 显示星期几
         // 获取当前月份$m共有多少天
-        $max = date('t', strtotime("$y-$m"));
-        $html .= '<tr>';                		// 开始<tr>标签
+        $max = date('t', strtotime("$y-$m")); // 获取指定年份和月份的总天数
+        $html .= '<tr>'; // 开始<tr>标签
         for ($d = 1; $d <= $max; ++$d) {
-            if ($w && $d == 1) {        		// 如果该月的第1天不是星期日，则填充空白
+            if ($w && $d == 1) { // 如果该月的第1天不是星期日，则填充空白
                 $html .= "<td colspan=\"$w\"> </td>";
             }
-            $html .= "<td>$d</td>";
-            if ($w == 6 && $d != $max) { 	    // 如果星期六不是该月的最后一天，则换行
+            $html .= "<td>$d</td>"; // 输出日期
+            if ($w == 6 && $d != $max) { // 如果星期六不是该月的最后一天，则换行
                 $html .= '</tr><tr>';
-            } elseif ($d == $max) {     		// 该月的最后一天，闭合<tr>标签
+            } elseif ($d == $max) { // 该月的最后一天，闭合<tr>标签
                 $html .= '</tr>';
             }
-            $w = ($w + 1 > 6) ? 0 : $w + 1;
+            $w = ($w + 1 > 6) ? 0 : $w + 1; // 更新星期几的值，保证不超过6
         }
         $html .= '</table>';
     }
     $html .= '</div>';
     return $html;
 }
-echo calendar('2024');
+echo calendar('2024'); // 调用函数生成2024年的年历
 ?>
 </body>
 </html>
